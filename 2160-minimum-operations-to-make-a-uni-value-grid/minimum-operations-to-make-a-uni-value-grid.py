08b60f71035fe26b269af25479cd3c088cd5c434
Aser Hailu <84772519+asero13th@users.noreply.github.com>
2026-06-18 13:11:28 +0300

    Time: 138 ms (62.68%) | Memory: 41.1 MB (44.59%) - LeetSync

--- 2160-minimum-operations-to-make-a-uni-value-grid/minimum-operations-to-make-a-uni-value-grid.py
+++ 2160-minimum-operations-to-make-a-uni-value-grid/minimum-operations-to-make-a-uni-value-grid.py
@@ -1,24 +1,19 @@
 class Solution:
     def minOperations(self, grid: List[List[int]], x: int) -> int:
         nums = []
+        rem = grid[0][0] % x
         for i in range(len(grid)):
             for j in range(len(grid[i])):
+                if grid[i][j] % x != rem:
+                    return -1
                 nums.append(grid[i][j])
+                
         nums.sort()
-
         middle = len(nums) // 2
-        rem = nums[0] % x
+        
         ans = 0
 
         for num in nums:
-            if num % x != rem:
-                return -1
-            # print(abs(middle - num) // x)
             ans += abs(nums[middle] - num) // x
 
         return ans
-
-
-
-
-
